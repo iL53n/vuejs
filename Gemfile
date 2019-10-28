dd6ed988e6b0a6313bf9070da229455242c01fd2
iL53n <ilsegribko@gmail.com>
2019-10-28 23:33:37 +0300

    pagination_sort - in progress

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'slim'
 gem 'uglifier', '>= 1.3.0'
 gem 'webpacker', '~> 4.x'
 gem 'pg_search'
+gem 'kaminari'
+gem 'fast_jsonapi'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/serializers/organization_serializer.rb
+++ app/serializers/organization_serializer.rb
@@ -1,4 +1,6 @@
-class OrganizationSerializer < ActiveModel::Serializer
+class OrganizationSerializer
+  include FastJsonapi::ObjectSerializer
+
   attributes :id, :title, :form_of_owership, :tax_number, :reg_number, :client_ids, :clients
   has_many :clients
   has_many :equipments
